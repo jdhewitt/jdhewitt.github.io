803adda28701fac0a85c5bcf4122b52b14098276
Darius McFarland <ddariusdev@gmail.com>
2025-11-28 10:13:52 -0500

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import { iconsSpritesheet } from "vite-plugin-icons-spritesheet"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig(({ mode }) => ({
-	base: "/jdhewitt.github.io/",
+	base: "/darius-0x4d.github.io/jdhewitt.github.io/",
 	plugins: [
 		tailwindcss(),
 		// Run the react-compiler on .tsx files only when bundling
